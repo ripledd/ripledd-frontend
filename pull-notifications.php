f97d82e37c81d0d3c35158d7cdf86cfc979b68b4
SpeecherTeam <105486423+SpeecherTeam@users.noreply.github.com>
2022-05-13 12:11:01 +0300

    Update pull-notifications.php

--- pull-notifications.php
+++ pull-notifications.php
@@ -1,1 +1,22 @@
+   <!-- function for user's new notifications ---->
+   <?php
+    $sql = "SELECT * FROM notifications_data WHERE user_s_id = '$secure_id' AND status = 'new'";
+     $rs = mysqli_query($dbconn, $sql);
+     //get row
+     $fetchRow = mysqli_fetch_assoc($rs);
+     $notification_status = $fetchRow['status'];
 
+      if ($notification_status == 'new') {
+        $notifi_red_dot_style = 'block';
+        }else {
+          $notifi_red_dot_style = 'none';
+        }
+        //get user data
+        $sql = "SELECT * FROM users WHERE secure_id = '$secure_id'";
+        $rs = mysqli_query($dbconn, $sql);
+        //get
+        $fetchRow = mysqli_fetch_assoc($rs);
+        $avatar = $fetchRow['avatar'];
+
+
+   ?>
