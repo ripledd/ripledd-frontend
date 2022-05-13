098d4b0d25f052cf9179d1cba84cc38af4837972
SpeecherTeam <105486423+SpeecherTeam@users.noreply.github.com>
2022-05-13 12:08:26 +0300

    Update feed.js

--- feed.js
+++ feed.js
@@ -1,1 +1,36 @@
-
+$(document).ready(function(){
+  var limit = 5;
+  var start = 0;
+  var action = 'inactive';
+  function load_data(limit, start){
+    $.ajax({
+     url:"posts-fetch.php",
+     method:"POST",
+     data:{limit:limit, start:start},
+     cache:false,
+     success:function(data){
+     $('#load_data').append(data);
+       if(data == ''){
+         $('#load_data_message').html("<p></p>");
+         action = 'active';
+         }else{
+         $('#load_data_message').html("<p></p>");
+           action = "inactive";
+        }
+       }
+      });
+     }
+     if(action == 'inactive'){
+      action = 'active';
+      load_data(limit, start);
+     }
+     $(window).scroll(function(){
+      if($(window).scrollTop() + $(window).height() > $("#load_data").height() - 2000 && action == 'inactive'){
+       action = 'active';
+       start = start + limit;
+        setTimeout(function(){
+         load_data(limit, start);
+        }, 1);
+       }
+      });
+     });
